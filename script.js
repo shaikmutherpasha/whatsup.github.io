9b6b2e3a149ea80b1895c0beb769c1918d8349b0
shaikmutherpasha <shaikmutherpasha@users.noreply.github.com>
2017-02-21 16:06:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
-    $("form").submit(function(){
-        var $message = $("#message").val();
-        alert($message);
-    });
-});
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://whatsup-43720.firebaseio.com/.json',
+        type: "GET",
+        data: JSON,
+        success: function(data) {
+            console.log(data);
+            $.each(data, function(i, v) {
 
+                $('#ol-list').append('<li>' + v + '</li>');
+            })
+        },
+        error: function(error) {
+            alert("error: something went wrong!");
+        }
+    });
+
+
+    $('#post-form').submit(function(event) {
+        event.preventDefault();
+        var $message = $("#message").val();
+        var url = $(this).attr('action');
+        $.post(url, { message: $message }).done(function(data) {
+            console.log(data);
+        })
+
+
+    });
+
+
+});
